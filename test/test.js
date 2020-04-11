e90356ba3633b459b33b0bd6232e6e268eaa2f5d
khimsarias <saumya27khimsaria@gmail.com>
2020-04-11 07:52:19 -0500

    update

---
+++ test/test.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+        
+        $(document).ready(function(){
+            
+                
+                    $(".pensive").hide();
+                    $(".block1").click(function(){
+                        $(".pensive").slideToggle(); 
+        });
+                
+                          });

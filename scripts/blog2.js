461332b375e76cc9ad335586903638fad425e089
Saumya Khimsaria <48251958+khimsarias@users.noreply.github.com>
2020-10-03 17:03:11 -0500

    leetcode

---
+++ scripts/blog2.js
@@ -0,0 +1,12 @@
+function leetcode(x)
+{
+    switch(x)
+    {
+        case 1:
+            window.open("project2.html#1", "_blank")
+            break;
+        case 7:
+            window.open("project2.html#7", "_blank")
+            break;
+    }
+}

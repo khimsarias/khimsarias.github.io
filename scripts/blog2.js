d3019c7d55c929fff7531614dac9b2e34ffef3a9
Saumya Khimsaria <48251958+khimsarias@users.noreply.github.com>
2020-10-06 10:48:33 -0500

    leetcode blog

--- scripts/blog2.js
+++ scripts/blog2.js
@@ -1,12 +1,5 @@
 function leetcode(x)
 {
-    switch(x)
-    {
-        case 1:
-            window.open("project2.html#1", "_blank")
-            break;
-        case 7:
-            window.open("project2.html#7", "_blank")
-            break;
-    }
+    let s1 = "project2.html#" + x;
+    window.open(s1, "_blank")
 }

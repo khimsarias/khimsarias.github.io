edeb7859118c83d7c3ce7ed4d11686368e485dfc
Saumya Khimsaria <48251958+khimsarias@users.noreply.github.com>
2021-03-16 11:24:30 -0500

    new design

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,38 @@ function scrollto(x)
     console.log(x);
 	document.getElementById(x).scrollIntoView(true);
 }
+
+function base()
+{
+    rt = document.getElementById("right");
+    lt = document.getElementById("left");
+    
+}
+function rightscroll()
+{
+    console.log("Right")
+    rt.addEventListener("scroll", (e2))
+}
+function rightscrollstop()
+{
+    rt.removeEventListener("scroll", (e2))
+}
+function leftscroll()
+{
+    console.log("Left")
+    lt.addEventListener("scroll", (e1))
+}
+
+function leftscrollstop()
+{
+    lt.removeEventListener("scroll", (e1))
+}
+function e2()
+{
+    lt.scroll(0,-rt.scrollTop)    
+        // console.log(rt.scrollTop)
+}
+function e1()
+{
+    rt.scroll(0,-lt.scrollTop)
+}
